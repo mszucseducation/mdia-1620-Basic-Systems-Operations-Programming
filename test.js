ea2911f8904e82b2ac529b38217153d25975422b
mszucseducation <mszucs.education@gmail.com>
2022-09-12 10:31:51 -0700

    testing

--- test.js
+++ test.js
@@ -1,14 +1,1 @@
-const person = {fname:"Jenn", pet:"dog"}; 
-let text = "";
-
-for (let x in person) {
-  if(x == "fname") {
-    text += person[x] + " has a pet ";
-  }
-  
-  if(x == "pet") {
-    text += person[x] + ".";
-  }
-}
-
-console.log(text)
+var test = 1;
