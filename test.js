074c9d69331609f349c6ec082381adbc49019d79
Monika Szucs <monika.szucs.work@gmail.com>
2022-09-16 12:15:26 -0700

    adding more code

--- test.js
+++ test.js
@@ -1,3 +1,31 @@
-// 
+// Boolean
+// Initialise the variable called proceed to the value true.
 
-var car;
+var proceed = true;
+var i = 0;
+
+while(proceed) {
+    i++; // i = 1;
+    proceed = false;
+};
+
+console.log(i);
+
+// Data Type - Object
+/**
+ * 6-4-1 Create a variable called grannySmith 
+ * and add the 
+ * following values (use camelCase):
+ * colour of the apple is green
+ * origin location is australia
+ * origin date is 1868
+ * Then console log out the colour from 
+ * the object.
+ */
+var grannySmith = {
+    color: "green",
+    originLocation: "australia",
+    originDate: 1868
+};
+
+console.log(grannySmith);
