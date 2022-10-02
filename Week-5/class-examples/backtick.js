62da4e03ab07a9231d3943628ee52eca55082378
Monika Szucs <monika.silvia.s@hotmail.ca>
2022-10-01 19:09:23 -0700

    Added class examples

---
+++ Week-5/class-examples/backtick.js
@@ -0,0 +1,78 @@
+/* 1
+ ? Initialize a variable called 
+ ~ supplies 
+ ? to the value of 
+ ~ pens
+ ? Initialize a variable called 
+ ~ quantity 
+ ? to the value of 
+ ~ 10
+ ?
+ ? Initialize a variable called 
+ ~ sentence 
+ ? to the sentence that says: (Use Backticks!)
+ ?
+ ^ I have 10 pens.
+ ? 
+ ? Then console log out this sentence
+ ? 
+ */
+
+
+
+
+
+
+
+
+/* 2
+ ? Initialize a variable called 
+ ~ first name 
+ ? to the value of 
+ ~ John
+ ? Initialize a variable called 
+ ~ last name 
+ ? to the value of 
+ ~ Doe
+ ?
+ ? Initialize a variable called 
+ ~ sentence 
+ ? to the sentence that says: (Use Backticks!)
+ ?
+ ^ John Doe is attending school today.
+ ? 
+ ? Then console log out this sentence
+ ? 
+ */
+
+
+
+
+
+
+
+
+ /* 3
+ ? Declare a variable called 
+ ~ calls 
+ ? Then assign calls to the value of: 
+ ~ 125
+ ? Declare a variable called 
+ ~ company 
+ ? Then assign company to the value of: 
+ ~ Telus Call Center
+ ?
+ ? Declare a variable called 
+ ~ sentence 
+ ? Then assign sentence to the sentence that says: (Use Backticks!)
+ ?
+ ^ Each person at the Telus Call Centre takes about 125 calls a day.
+ ? 
+ ? Then console log out this sentence
+ & Put all Declarations at the top of the page followed by the assignment. 
+ */
+
+
+
+
+ 
