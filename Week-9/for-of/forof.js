9ca938ae826ac8bd4016e5445221d5507f677780
Monika Szucs <monika.szucs.work@gmail.com>
2022-10-18 22:02:01 -0700

    Added Week 9 content

---
+++ Week-9/for-of/forof.js
@@ -0,0 +1,8 @@
+var numbers = [2, 4, 6];
+var total = 0;
+
+for (var num of numbers) {
+    total += num ;
+}
+
+console.log(total);
---
+++ Week-9/for/example2.js
@@ -0,0 +1,10 @@
+var text = "";
+
+for (var i = 0; i < 5; i++) {
+  if(i == 3) {
+    text += "Finished";
+    break;
+  }
+}
+
+console.log(text + " at the number " + i);
